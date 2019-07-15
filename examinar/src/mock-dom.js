9599eb4c62f589b5513c7d625df5d61e87b71178
Carl Mungazi <carlmungazi@Carls-MacBook-Pro.local>
2019-07-15 20:34:21 +0100

    Change DOM implementation

--- examinar/src/mock-dom.js
+++ examinar/src/mock-dom.js
@@ -1,41 +1,32 @@
 "use strict";
 
 const document = {
-  _ids: new Map(),
+  _elementIds: {},
   createElement(tag) {
     const element = {
       nodeType: 1,
       nodeName: tag.toUpperCase(),
-      namespaceURI: 'http://www.w3.org/1999/xhtml',
       parentNode: null,
       childNodes: [],
       appendChild,
       setAttribute,
       attributes: {},
-      $$dom: null
-    }
+      $$dom: true
+    };
 
     return element;
   },
   createTextNode(text) {
-    const nodeVal = text;
-
     return {
       nodeType: 3,
       nodeName: "#text",
       parentNode: null,
-      get nodeVal() {
-        return nodeVal;
-      },
-      set nodeVal(val) {
-        nodeVal = val
-      }
+      data: text
     }
   },
   getElementById(id) {
-    // implement a hash map look up: https://stackoverflow.com/questions/2711303/javascript-getelementbyid-lookups-hash-map-or-recursive-tree-traversal
-    if (this._ids.has(id)) {
-      return this._ids.get(id)
+    if (document._elementIds[id]) {
+      return document._elementIds[id]
     }
 
     return null;
@@ -45,27 +36,24 @@ const document = {
 function appendChild(child) {
   let ancestor = this;
 
-  while (ancestor !== child && ancestor !== null) ancestor = ancestor.parentNode
-  if (ancestor === child) throw new Error("Ancestor cannot be equal to child")
-  if (child.nodeType == null) throw new Error("The child is not a DOM element")
+  if (ancestor === child) throw new Error("Child element cannot be equal to parent element");
+  if (child.nodeType == null) throw new Error("The child is not a DOM element");
 
   const index = this.childNodes.indexOf(child);
-  if (index > -1 ) this.childNodes.splice(index, 1)
+  if (index > -1 ) this.childNodes.splice(index, 1);
 
   this.childNodes.push(child);
-  if (child.parentNode != null && child.parentNode !== this) child.parentNode.removeChild(child)
-  child.parentNode = this;
+  child.parentNode = ancestor;
 }
 
 function setAttribute(name, value) {
   this.attributes[name] = value;
-
-  if (name === 'id' && document._ids.has(value)) {
-    throw new Error(`${value} is already the id of another element`);
-  }
   
-  if (name === 'id' && !document._ids.has(value)) {
-    document._ids.set(value, this)
+  if (name === 'id') {
+    if (document._elementIds[value]) {
+      throw new Error(`${value} is already the id of an existing element`);
+    }
+    document._elementIds[value] = this;
   }
 }
 
